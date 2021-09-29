0f0b1564d310a9f87dcadde4940bf038dc036636
Mark <louw.mark@yahoo.com>
2021-09-29 07:44:37 +0200

    Vector Icons
    Added vector icons to project. Added new chat button, functionality needs to be implemeneted.

--- Appmo/screens/HomeScreen.js
+++ Appmo/screens/HomeScreen.js
@@ -1,9 +1,15 @@
 import React, {useEffect, useState} from 'react';
-import {StyleSheet, Button, View} from 'react-native';
+import {StyleSheet, Button, View, TouchableOpacity} from 'react-native';
 import auth from '@react-native-firebase/auth';
 import SocketService from '../SocketService';
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 const HomeScreen = () => {
+  const NewChatButton = () => {
+    console.log('You have been clicked a button!');
+    // do something
+  };
+
   async function SignOutGoogle() {
     return auth().signOut();
   }
@@ -16,13 +22,32 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <View>
-      <Button onPress={SocketService.Send} title="Say Hello" />
-      <Button onPress={SignOutGoogle} title="Sign Out" />
+    <View style={styles.Screen}>
+      <TouchableOpacity
+        onPress={NewChatButton}
+        style={styles.NewChatButton}>
+        <MaterialCommunityIcons name="chat-plus-outline" size={24} color="black" />
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  Screen: {
+    flex: 1,
+  },
+  NewChatButton: {
+    height: 60,
+    width: 60,
+    borderRadius: 100,
+    backgroundColor: "#99AAAB",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+    alignSelf: "flex-end"
+  },
+});
