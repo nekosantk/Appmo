b3404a3ee96e818e357f5eebd88d5a445e869784
Mark <louw.mark@yahoo.com>
2021-09-30 00:00:58 +0200

    Fixed NewChat screen profile pics
    Profile picture uri now correctly gets passed to custom panel component.

--- Appmo/components/ContactPanel.js
+++ Appmo/components/ContactPanel.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Avatar, ListItem } from 'react-native-elements'
 
-const ContactPanel = ({ key, id, displayName, avatarUrl, statusMessage }) => {
+const ContactPanel = ({ id, displayName, avatarUrl, statusMessage }) => {
     return (
         <ListItem key={id}>
             <Avatar 
                 rounded
                 source={{
-                    uri: 'https://wallpapercave.com/wp/wp6409639.jpg',
+                    uri: avatarUrl,
                 }}
             />
             <ListItem.Content>
@@ -16,7 +16,7 @@ const ContactPanel = ({ key, id, displayName, avatarUrl, statusMessage }) => {
                     {displayName}
                 </ListItem.Title>
                 <ListItem.Subtitle>
-                    {avatarUrl}
+                    {statusMessage}
                 </ListItem.Subtitle>
             </ListItem.Content>
         </ListItem>
